a9edaac8e1807e3411ea2d452eeb1657787c73fc
e1b22063 <e1b22063@oit.ac.jp>
2024-10-01 14:37:11 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/lec02/kadai/lec02/Lec02Application.java
@@ -0,0 +1,13 @@
+package oit.is.lec02.kadai.lec02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lec02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lec02Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/lec02/kadai/lec02/Lec02ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.lec02.kadai.lec02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lec02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

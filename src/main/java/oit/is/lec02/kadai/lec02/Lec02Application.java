f25d6b47edbfb6c83c9a934b2ca1377424664c98
e1b22027 <e1b22027@oit.ac.jp>
2024-10-01 15:58:12 +0900

    Sample2-1の実装

--- src/main/java/oit/is/lec02/kadai/lec02/Lec02Application.java
+++ src/main/java/oit/is/lec02/kadai/lec02/Lec02Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Lec02Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Lec02Application.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(Lec02Application.class, args);
+  }
 
 }
